4914439c1fb62675ef1337750c6aadd8c8a65b8e
mosavimaedehhh <152817577+mosavimaedehhh@users.noreply.github.com>
2023-12-04 09:24:21 +0300

    Add files via upload

---
+++ مائده موسوی س 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	float m,n,A,B,C,D;
+	cout<<"pleaese enter tow decimal numbers ";
+	cin>>m>>n;
+	A=m+n;
+	
+	B=m*n;
+	
+	D=m/n;
+	
+	cout<<"plus"<< A <<"/n"<<"minus"<< B <<"/n"<<"multiply"<< C <<"/n"<<"divided by"<< D;
+}
